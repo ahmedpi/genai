61322867c61252d98aa4decc091d11bac309817a
AhmedAli Mohammed <ahmedali_mohammed@epam.com>
2025-03-14 19:30:46 +0100

    revert latest changes

--- src/main/java/com/epam/training/gen/ai/service/OpenAIService.java
+++ src/main/java/com/epam/training/gen/ai/service/OpenAIService.java
@@ -29,52 +29,18 @@ public class OpenAIService {
   private String model;
 
   public BookResponse processBookPrompt(String prompt) {
-    //Uncomment to use change message
-//    String prompt = """
-//        I want to find top-10 books about world history
-//         """;
-    ChatHistory history = new ChatHistory();
-    history.addUserMessage(prompt);
-    List<ChatMessageContent<?>> response = getCompletion(prompt, history);
-
-    return processBookResponse(response, history);
-  }
-
-  public String summarizeText(String text) {
-    //Uncomment to change user message
-//    text = """
-//        Making a cup of tea is easy! First, you need to get some\nwater boiling. While that's happening\\ngrab a cup and put a tea bag in it. Once the water is\nhot enough, just pour it over the tea bag.\\nLet it sit for a bit so the tea can steep. After a\nfew minutes, take out the tea bag. If you \nlike, you can add some sugar or milk to taste.\nAnd that's it! You've got yourself a delicious \ncup of tea to enjoy
-//        """;
-
-    String prompt = """
-        You will be provided with text delimited by triple quotes. 
-        If it contains a sequence of instructions, 
-        re-write those instructions in the following format:
-
-        Step 1 - ...
-        Step 2 - …
-        …
-        Step N - …
-
-        If the text does not contain a sequence of instructions, 
-        then simply write "No steps provided."
-
-        \"\"\"""" + text + "\"\"\"";
-    ChatHistory history = new ChatHistory();
-    history.addUserMessage(prompt);
-    List<ChatMessageContent<?>> response = getCompletion(prompt, history);
-
-    return processResponse(response, history);
-  }
-
-  private List<ChatMessageContent<?>> getCompletion(String prompt, ChatHistory history) {
     OpenAIAsyncClient client = createOpenAIAsyncClient();
 
     ChatCompletionService chatCompletionService = createChatCompletionService(client);
     Kernel kernel = createKernel(chatCompletionService);
 
-    return fetchChatResponse(chatCompletionService, history,
+    ChatHistory history = new ChatHistory();
+    history.addUserMessage(prompt);
+
+    List<ChatMessageContent<?>> response = fetchChatResponse(chatCompletionService, history,
         kernel);
+
+    return processResponse(response, history);
   }
 
   private OpenAIAsyncClient createOpenAIAsyncClient() {
@@ -113,8 +79,7 @@ public class OpenAIService {
         optionalInvocationContext).block();
   }
 
-  private BookResponse processBookResponse(List<ChatMessageContent<?>> response,
-      ChatHistory history) {
+  private BookResponse processResponse(List<ChatMessageContent<?>> response, ChatHistory history) {
     if (response == null || response.isEmpty()) {
       return new BookResponse("No response received from the assistant.");
     }
@@ -128,21 +93,5 @@ public class OpenAIService {
 
     // Return the first assistant message as the response
     return new BookResponse(response.get(0).getContent());
-  }
-
-  private String processResponse(List<ChatMessageContent<?>> response, ChatHistory history) {
-    if (response == null || response.isEmpty()) {
-      return "No response received from the assistant.";
-    }
-
-    for (ChatMessageContent<?> result : response) {
-      if (result.getAuthorRole() == AuthorRole.ASSISTANT && result.getContent() != null) {
-        System.out.println("Assistant > " + result);
-        history.addMessage(result); // Add assistant's message to chat history
-      }
-    }
-
-    // Return the first assistant message as the response
-    return response.get(0).getContent();
   }
 }
